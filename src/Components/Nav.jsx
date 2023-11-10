d845bab368a64fda1ba37712c7a0d81fec403d17
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-11-10 09:39:26 -0500

    added the pages

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import Home from "./Pages/Home";
+
 const Nav = () => {
   return (
     <>
-      <div className="navBar"></div>
+      <nav className="nav">
+        <Link to="/">SpendWise</Link>
+        <button>New Transaction</button>
+        <h1></h1>
+      </nav>
     </>
   );
 };
+
+export default Nav;
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <div className="home-page">
+        <h1>Welcome To The Home Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
